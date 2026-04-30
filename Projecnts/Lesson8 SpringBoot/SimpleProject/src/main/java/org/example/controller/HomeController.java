474eb5919ab09dce8bdda1c925156144deecb4fa
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-04-30 17:50:34 +0300

    Spring boot mvc

--- Projecnts/Lesson8 SpringBoot/SimpleProject/src/main/java/org/example/controller/HomeController.java
+++ Projecnts/Lesson8 SpringBoot/SimpleProject/src/main/java/org/example/controller/HomeController.java
@@ -1,12 +1,21 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.repositories.IGenreRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final IGenreRepository genreRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        //список усіх жанрів
+        var list = genreRepository.findAll();
+        model.addAttribute("genres",list);
         return "index";
     }
 }
