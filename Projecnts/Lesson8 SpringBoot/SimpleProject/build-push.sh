6748f6c356747dc304a0d00b4b2771cd502d33f6
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-05-09 16:19:59 +0300

    edit .sh

--- Projecnts/Lesson8 SpringBoot/SimpleProject/build-push.sh
+++ Projecnts/Lesson8 SpringBoot/SimpleProject/build-push.sh
@@ -3,15 +3,15 @@
 echo "Docker login..."
 docker login
 
-echo "Building Docker image api..."
+echo "Building Docker image mvc..."
 docker build -t p32-java .
 
-echo "Tagging Docker image api..."
+echo "Tagging Docker image mvc..."
 docker tag p32-java:latest avalentyn/p32-java:latest
 
-echo "Pushing Docker image api to repository..."
+echo "Pushing Docker image mvc to repository..."
 docker push avalentyn/p32-java:latest
 
-echo "Done ---api---!"
+echo "Done ---mvc---!"
 read -p "Press Enter to continue..."
  
